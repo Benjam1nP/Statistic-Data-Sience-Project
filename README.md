8abfaf5649fab487617f06ab3022032412dc29fb
Benjam1nP <benjamin.pfister@outlook.com>
2025-09-30 23:47:49 +0200

    implement the change to datetime and clean up dataset

---
+++ README.md
@@ -0,0 +1,4 @@
+** Setup the Dataset **
+1. execute delete_columns.py 
+2. execute change_to_datetime.py (might take 30min)
+3. Taxi_final is the clean dataset
---
+++ chane_to_datetime.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+input_file = "Taxi_Data_improved.csv"
+output_file = "Taxi_final.csv"
+chunksize = 2_000_000
+
+first_chunk = True
+
+for chunk in pd.read_csv(input_file, chunksize=chunksize, low_memory=False):
+    # Spalten in echtes datetime64 umwandeln
+    chunk["tpep_pickup_datetime"] = pd.to_datetime(chunk["tpep_pickup_datetime"], errors="coerce")
+    chunk["tpep_dropoff_datetime"] = pd.to_datetime(chunk["tpep_dropoff_datetime"], errors="coerce")
+
+    # ins neue CSV schreiben
+    chunk.to_csv(output_file, mode="a", index=False, header=first_chunk)
+    first_chunk = False
